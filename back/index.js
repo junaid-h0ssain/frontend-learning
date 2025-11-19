e0cdaa43f03ce2cb18963cbe922d03851b591ae1
Junaid <158271661+junaid-h0ssain@users.noreply.github.com>
2025-11-19 21:12:07 +0600

    express setup

---
+++ back/index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+// get note
+function getNote(req,res){
+    let c = 5
+    res.status(200).send("You got "+ c +" notes");
+}
+app.get("/api/notes",getNote);
+
+// post note
+function postNote(req,res){
+    let c = "New Note wawa"
+    res.status(201).json({message : ""+c+ " posted successfully"});
+}
+app.post("/api/notes",postNote);
+
+// put note
+function putNote(req,res){
+    res.status(200).json({message : ""+c+ " post updated successfully"});
+}
+app.put("/api/notes/:id",putNote);
+
+// delete note
+function deleteNote(req,res){
+    res.status(200).json({message : " post deleted successfully"});
+}
+app.delete("/api/notes/:id",deleteNote);
+
+
+// server listening
+function hello () {
+    console.log("Server running on port 8080");
+}
+app.listen(5001, hello);
