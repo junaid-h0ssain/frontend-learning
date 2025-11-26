fefed35589cec8e0c1649517c69f9aa6fc4f83a8
Junaid <158271661+junaid-h0ssain@users.noreply.github.com>
2025-11-26 16:34:28 +0600

    added home to vite build

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        home: resolve(__dirname, 'home.html'),
+      },
+    },
+  },
 })
